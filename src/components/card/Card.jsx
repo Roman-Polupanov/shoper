7502372fe95b4a9de2e5815e3ff6ef43ebf2ad13
Roman Polupanov <87490407+Roman-Polupanov@users.noreply.github.com>
2022-02-18 12:59:56 +0200

    likes trying

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,26 @@
 import { Card_uno, Good_name, Button_price, Price, Good_image, Good_price } from './Card.styles';
+import data from '../../mock/data';
+import React, { useState } from 'react';
 
-const Card = ({ id, name, image, price }) => {
+const Card = ({ id, name, image, price, like }) => {
+    const [list, setLike] = useState(data);
+
+    const handleClick = () => {
+        const draftList = [...list];
+        setLike(draftList)
+    }
+
     return (<Card_uno key={id}>
         <Good_name>{name}</Good_name>
         <Good_image src={image} />
         <Button_price>
             <Good_price>{price}$</Good_price>
             <Price>buy</Price>
-            <Price>like it</Price>
+
+            <Price onClick={() => handleClick(id)}>
+                {like ? "like me" : "unlike me"}
+            </Price>
+
         </Button_price>
     </Card_uno>
     )
